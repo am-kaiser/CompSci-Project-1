error in order calculation fixed

regression_analysis/fit_model/ordinary_least_squares.py
@@ -35,13 +35,13 @@ def perform_OLS(input_x1, input_x2, obs_var, order, train_frac, bootstrap=False)
         The train ratio is the fraction of data that is used as training data.
         Additional parameter enables bootstrap resampling.
     """
-    if bootstrap:
-        # Perform bootstrap
-        input_x1, input_x1, obs_var = prepare_data.bootstrap(input_x1, input_x2, obs_var)
-
     # Split data in test and train datasets
     x1_train, x2_train, x1_test, x2_test, y_train, y_test = prepare_data.split_test_train(input_x1, input_x2, obs_var,
                                                                                           train_fraction=train_frac)
+
+    if bootstrap:
+        # Perform bootstrap
+        x1_train, x2_train, y_train = prepare_data.bootstrap(x1_train, x2_train, y_train)
 
     # Calculate beta and response variable for train dataset
     beta_OLS_train, resp_var_OLS_train = calculate_OLS(x1_train, x2_train, y_train, order)
@@ -69,15 +69,18 @@ if __name__ == "__main__":
                                                                    points=100)
 
     # Fit model for different polynomial
-    max_order = 5
+    max_order = 2
     orders = range(1, max_order+1)
     MSE_train = np.empty([1, max_order])
     R2_train = np.empty([1, max_order])
     MSE_test = np.empty([1, max_order])
     R2_test = np.empty([1, max_order])
 
-    for index, order in enumerate(orders):
-        _, _, _, MSE_train[:, index], MSE_test[:, index], R2_train[:, index], R2_test[:, index] = perform_OLS(input_x1, input_x2, obs_var, order, train_frac=0.8, bootstrap=True)
+    for i, order in enumerate(orders):
+        _, _, _, MSE_train[:, i], MSE_test[:, i], R2_train[:, i], R2_test[:, i] = perform_OLS(input_x1, input_x2,
+                                                                                              obs_var, order,
+                                                                                              train_frac=0.8,
+                                                                                              bootstrap=True)
 
     # Plot errors
     axes_1 = np.array(orders)

regression_analysis/tests/test_ordinary_least_squares.py
@@ -9,30 +9,32 @@ from regression_analysis.utils import basis_functionality
 
 def test_correct_beta():
     # Intercept is included in the design matrix
-    design_matrix = np.array([[1, 3, 2, 6], [1, 5, 4, 20], [1, 7, 8, 56], [1, 9, 10, 90]])
+    design_matrix = np.array([[1, 3, 2], [1, 5, 4], [1, 7, 8], [1, 9, 10]])
     obs_var = np.array([10, 3, 7, 15]).reshape(4, 1)
 
     # Calculate beta with scikit-learn
     OLS_reg_sklearn = LinearRegression(fit_intercept=False).fit(design_matrix, obs_var)
     beta_sklearn = OLS_reg_sklearn.coef_
     # Calculate beta with own code
-    beta_OLS_own, _ = ordinary_least_squares.calculate_OLS(design_matrix[:, 2], design_matrix[:, 1], obs_var=obs_var, order=1)
+    beta_OLS_own, _ = ordinary_least_squares.calculate_OLS(design_matrix[:, 2], design_matrix[:, 1], obs_var=obs_var,
+                                                           order=1)
 
     assert beta_sklearn.all() == beta_OLS_own.all()
 
 
 def test_errors():
     # Intercept is included in the design matrix
-    design_matrix = np.array([[1, 3, 2, 6], [1, 5, 4, 20], [1, 7, 8, 56], [1, 9, 10, 90]])
+    design_matrix = np.array([[1, 3, 2], [1, 5, 4], [1, 7, 8], [1, 9, 10]])
     obs_var = np.array([10, 3, 7, 15]).reshape(4, 1)
 
     # Calculate response variable with own code
-    _, resp_var_OLS = ordinary_least_squares.calculate_OLS(design_matrix[:, 2], design_matrix[:, 1], obs_var=obs_var, order=1)
+    _, resp_var_OLS = ordinary_least_squares.calculate_OLS(design_matrix[:, 2], design_matrix[:, 1], obs_var=obs_var,
+                                                           order=1)
 
-    # Calculate means squared error with scikit-learn
+    # Calculate mean squared error with scikit-learn
     MSE_sklearn = mean_squared_error(obs_var, resp_var_OLS)
-    print(MSE_sklearn)
-    # Calculate means squared error with own code
+
+    # Calculate mean squared error with own code
     error_class = basis_functionality.Error_Measures(obs_var, resp_var_OLS)
     MSE_own = error_class.mean_squared_error()
 

regression_analysis/utils/basis_functionality.py
@@ -18,7 +18,9 @@ class Design_Matrix_2D:
     def make_ploynomial_power(self):
         """Create array with the powers of the polynomial which will be fitted."""
         order_range = range(self.order+1)
-        return np.array(np.meshgrid(order_range, order_range)).T.reshape(-1, 2)
+        order_options = np.array(np.meshgrid(order_range, order_range)).T.reshape(-1, 2)
+        row_sum = np.sum(order_options, axis=1) <= self.order
+        return order_options[row_sum, :]
 
     def make_design_matrix(self):
         """Define design matrix D for given input. Order is the order of the polynomial which will be