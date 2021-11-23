b42bfa62db803c1e5bc97c59fe1653b7a2a351ea
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2021-11-23 14:43:17 +0100

    added lasso regression

--- regression_analysis/fit_model/lasso_regression.py
+++ regression_analysis/fit_model/lasso_regression.py
@@ -1,4 +1,4 @@
-"""Script to calculate Ridge Regression."""
+"""Script to calculate Lasso Regression."""
 
 from regression_analysis.utils import create_data_franke
 from regression_analysis.utils import basis_functionality
@@ -7,9 +7,10 @@ from regression_analysis.utils import prepare_data
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import linear_model
 
 
-def calculate_RR(input_x1, input_x2, obs_var, order, lam, D=None, beta_RR=None):
+def calculate_LR(input_x1, input_x2, obs_var, order, lam, D=None):
     """Calculate the response variable for Ridge Regression. lam stands for lambda."""
 
     if not(D):
@@ -17,21 +18,16 @@ def calculate_RR(input_x1, input_x2, obs_var, order, lam, D=None, beta_RR=None):
         regress_obj = basis_functionality.Design_Matrix_2D(input_x1, input_x2, obs_var, order)
         D = regress_obj.make_design_matrix()
 
-    if not(beta_RR):
-        # Calculate parameter vector with given design matrix
-        regress_obj = basis_functionality.Design_Matrix_2D(input_x1, input_x2, obs_var, order)
-        temp_matrix = np.dot(D.T, D) + lam*np.identity(D.shape[1])
-        beta_RR = np.dot(np.dot(regress_obj.calculate_matrix_inverse(temp_matrix), D.T),  obs_var.flatten())
+    model_lasso = linear_model.Lasso(alpha=lam, fit_intercept=False).fit(D, obs_var.flatten())
+    beta_LR = model_lasso.coef_
+    resp_var_LR = model_lasso.predict(D)
 
-    # Calculate response variable
-    resp_var_RR = np.dot(D, beta_RR).reshape(obs_var.shape[0], obs_var.shape[1])
-
-    return beta_RR, resp_var_RR
+    return beta_LR, resp_var_LR.reshape(obs_var.shape[0], obs_var.shape[1])
 
 
-def perform_RR(input_x1, input_x2, obs_var, order, lam, train_frac):
+def perform_LR(input_x1, input_x2, obs_var, order, lam, train_frac):
     """
-    Performs Ridge Regression for a 2D function using polynomials of order "order".
+    Performs Lasso Regression for a 2D function using polynomials of order "order".
     The train ratio is the fraction of data that is used as training data.
     """
     # Step1: Scale data
@@ -46,31 +42,32 @@ def perform_RR(input_x1, input_x2, obs_var, order, lam, train_frac):
                                                                                           train_fraction=train_frac,
                                                                                           do_shuffle=True)
 
-    # Step 3: Calculate Ridge Regression
+    # Step 3: Calculate Lasso Regression
     # Calculate beta and response variable for train dataset
-    beta_RR_train, resp_var_RR_train = calculate_RR(x1_train, x2_train, y_train, order, lam=lam)
+    beta_LR_train, resp_var_LR_train = calculate_LR(x1_train, x2_train, y_train, order, lam=lam)
+
     # Calculate error evaluaters for the train dataset
-    error_class = basis_functionality.Error_Measures(y_train, resp_var_RR_train)
+    error_class = basis_functionality.Error_Measures(y_train, resp_var_LR_train)
     MSE_train = error_class.mean_squared_error()
     R2_train = error_class.r2_score()
 
-    # Get Ridge Regression response variable for test dataset
+    # Get Lasso Regression response variable for test dataset
     regress_obj_test = basis_functionality.Design_Matrix_2D(x1_test, x2_test, y_test, order)
     D_test = regress_obj_test.make_design_matrix()
-    resp_var_RR_test = np.dot(D_test, beta_RR_train).reshape(y_test.shape[0], y_test.shape[1])
+    resp_var_LR_test = np.dot(D_test, beta_LR_train).reshape(y_test.shape[0], y_test.shape[1])
 
     # Step 4: Calculate errors
     # Calculate error evaluaters for the test dataset
-    error_class = basis_functionality.Error_Measures(y_test, resp_var_RR_test)
+    error_class = basis_functionality.Error_Measures(y_test, resp_var_LR_test)
     MSE_test = error_class.mean_squared_error()
     R2_test = error_class.r2_score()
 
     return MSE_train, MSE_test, R2_train, R2_test
 
 
-def perform_RR_cross_val(input_x1, input_x2, obs_var, order, lam, num_fold=5):
+def perform_LR_cross_val(input_x1, input_x2, obs_var, order, lam, num_fold=5):
     """
-    Performs ridge regression for a 2D function using polynomials of order "order".
+    Performs Lasso Regression for a 2D function using polynomials of order "order".
     Additional parameter specifies number of folds for cross_validation.
     """
     # Step1: Scale data
@@ -89,7 +86,7 @@ def perform_RR_cross_val(input_x1, input_x2, obs_var, order, lam, num_fold=5):
     x_train_arr, x_test_arr, y_train_arr, y_test_arr = prepare_data.cross_validation(input_x1, input_x2, obs_var,
                                                                                      num_fold)
 
-    # Step 3: Calculate Ridge Regression for each fold
+    # Step 3: Calculate LR for each fold
     for fold_index in range(num_fold):
 
         x1_train = x_train_arr[fold_index, :, 0]
@@ -102,21 +99,21 @@ def perform_RR_cross_val(input_x1, input_x2, obs_var, order, lam, num_fold=5):
         y_test = y_test_arr[fold_index, :].reshape(len(y_test_arr[fold_index, :]), 1)
 
         # Calculate beta and response variable for train dataset
-        beta_RR_train, resp_var_RR_train = calculate_RR(x1_train, x2_train, y_train, order, lam=lam)
+        beta_LR_train, resp_var_LR_train = calculate_LR(x1_train, x2_train, y_train, order, lam=lam)
         # Calculate error evaluaters for the train dataset
-        error_class = basis_functionality.Error_Measures(y_train, resp_var_RR_train)
+        error_class = basis_functionality.Error_Measures(y_train, resp_var_LR_train)
 
         MSE_train[0, fold_index] = error_class.mean_squared_error()
         R2_train[0, fold_index] = error_class.r2_score()
 
-        # Get RR response variable for test dataset
+        # Get LR response variable for test dataset
         regress_obj_test = basis_functionality.Design_Matrix_2D(x1_test, x2_test, y_test, order)
         D_test = regress_obj_test.make_design_matrix()
-        resp_var_RR_test = np.dot(D_test, beta_RR_train).reshape(y_test.shape[0], y_test.shape[1])
+        resp_var_LR_test = np.dot(D_test, beta_LR_train).reshape(y_test.shape[0], y_test.shape[1])
 
         # Step 4: Calculate errors for each fold
         # Calculate error evaluaters for the test dataset
-        error_class = basis_functionality.Error_Measures(y_test, resp_var_RR_test)
+        error_class = basis_functionality.Error_Measures(y_test, resp_var_LR_test)
         MSE_test[0, fold_index] = error_class.mean_squared_error()
         R2_test[0, fold_index] = error_class.r2_score()
 
@@ -128,9 +125,9 @@ def perform_RR_cross_val(input_x1, input_x2, obs_var, order, lam, num_fold=5):
     return MSE_train_mean, MSE_test_mean, R2_train_mean, R2_test_mean
 
 
-def perform_RR_bootstrap(input_x1, input_x2, obs_var, order, lam, train_frac=0.8, num_boot=5):
+def perform_LR_bootstrap(input_x1, input_x2, obs_var, order, lam, train_frac=0.8, num_boot=5):
     """
-    Performs ridge regression for a 2D function using polynomials of order "order".
+    Performs lasso regression for a 2D function using polynomials of order "order".
     Additional parameter specifies number of number of times bootstrap will be performed.
     """
     # Step1: Scale data
@@ -151,28 +148,28 @@ def perform_RR_bootstrap(input_x1, input_x2, obs_var, order, lam, train_frac=0.8
                                                                                           train_fraction=train_frac,
                                                                                           do_shuffle=True)
 
-    # Step 3: Calculate RR for bootstrap
+    # Step 3: Calculate LR for bootstrap
     for boot_index in range(num_boot):
 
         # Perform Boostrapping on training data
         x1_train_boot, x2_train_boot, y_train_boot = prepare_data.bootstrap(x1_train, x2_train, y_train)
 
         # Calculate beta and response variable for train dataset
-        beta_RR_train, resp_var_RR_train = calculate_RR(x1_train_boot, x2_train_boot, y_train_boot, order, lam=lam)
+        beta_LR_train, resp_var_LR_train = calculate_LR(x1_train_boot, x2_train_boot, y_train_boot, order, lam=lam)
         # Calculate error evaluaters for the train dataset
-        error_class = basis_functionality.Error_Measures(y_train_boot, resp_var_RR_train)
+        error_class = basis_functionality.Error_Measures(y_train_boot, resp_var_LR_train)
 
         MSE_train[0, boot_index] = error_class.mean_squared_error()
         R2_train[0, boot_index] = error_class.r2_score()
 
-        # Get RR response variable for test dataset
+        # Get LR response variable for test dataset
         regress_obj_test = basis_functionality.Design_Matrix_2D(x1_test, x2_test, y_test, order)
         D_test = regress_obj_test.make_design_matrix()
-        resp_var_RR_test = np.dot(D_test, beta_RR_train).reshape(y_test.shape[0], y_test.shape[1])
+        resp_var_LR_test = np.dot(D_test, beta_LR_train).reshape(y_test.shape[0], y_test.shape[1])
 
         # Step 4: Calculate errors for each fold
         # Calculate error evaluaters for the test dataset
-        error_class = basis_functionality.Error_Measures(y_test, resp_var_RR_test)
+        error_class = basis_functionality.Error_Measures(y_test, resp_var_LR_test)
         MSE_test[0, boot_index] = error_class.mean_squared_error()
         R2_test[0, boot_index] = error_class.r2_score()
 
@@ -188,7 +185,7 @@ if __name__ == "__main__":
 
     # Get data
     input_x1, input_x2, obs_var = create_data_franke.generate_data(noisy=False, noise_variance=0.5, uniform=False,
-                                                                   points=100)
+                                                                   points=5)
 
     # Fit model for different polynomial
     max_order = 5
@@ -210,19 +207,19 @@ if __name__ == "__main__":
     R2_test_b = np.empty([1, max_order])
 
     for i, order in enumerate(orders):
-        MSE_train[:, i], MSE_test[:, i], R2_train[:, i], R2_test[:, i] = perform_RR(input_x1, input_x2, obs_var, order,
-                                                                                    lam=0.2, train_frac=0.8)
-        MSE_train_c[:, i], MSE_test_c[:, i], R2_train_c[:, i], R2_test_c[:, i] = perform_RR_cross_val(input_x1,
+        MSE_train[:, i], MSE_test[:, i], R2_train[:, i], R2_test[:, i] = perform_LR(input_x1, input_x2, obs_var, order,
+                                                                                    lam=2, train_frac=0.8)
+        MSE_train_c[:, i], MSE_test_c[:, i], R2_train_c[:, i], R2_test_c[:, i] = perform_LR_cross_val(input_x1,
                                                                                                       input_x2,
                                                                                                       obs_var,
                                                                                                       order,
-                                                                                                      lam=0.2,
+                                                                                                      lam=2,
                                                                                                       num_fold=5)
-        MSE_train_b[:, i], MSE_test_b[:, i], R2_train_b[:, i], R2_test_b[:, i] = perform_RR_bootstrap(input_x1,
+        MSE_train_b[:, i], MSE_test_b[:, i], R2_train_b[:, i], R2_test_b[:, i] = perform_LR_bootstrap(input_x1,
                                                                                                       input_x2,
                                                                                                       obs_var,
                                                                                                       order,
-                                                                                                      lam=0.2,
+                                                                                                      lam=2,
                                                                                                       train_frac=0.8,
                                                                                                       num_boot=5)
 
