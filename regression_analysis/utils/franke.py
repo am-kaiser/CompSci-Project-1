e5654bfad4c2c54c4370bd52d143fb3fdf15b726
harishpjain <harishjain41@gmail.com>
2021-12-07 13:07:19 +0100

    fixed that below zero noise was not available. experiments only updated for OLS. Need to do Ridge and Lasso

--- regression_analysis/utils/franke.py
+++ regression_analysis/utils/franke.py
@@ -11,7 +11,7 @@ def Franke(x1, x2, noise_var=0.0):
     #note if we don't truncate the gaussian, we won't see the effect of the noise 
     #as all noise will be rescaled to the range 0,1.
     e[np.where(e>1.0)]=1.0
-    e[np.where(e<0.0)]=0.0
+    e[np.where(e<-1.0)]=-1.0
     return a + b + c - d + e
 
 
@@ -26,6 +26,17 @@ def create_data(num_points, noise_variance):
 
     return xx1, xx2, y
 
+def Franke_and_biascorrection(x1, x2, noise_var=0.0):
+    a = 0.75 * np.exp(-((9 * x1 - 2) ** 2) / 4 - ((9 * x2 - 2) ** 2) / 4)
+    b = 0.75 * np.exp(-((9 * x1 + 1) ** 2) / 49 - (9 * x2 + 1) / 10)
+    c = 0.5 * np.exp(-((9 * x1 - 7) ** 2) / 4 - ((9 * x2 - 3) ** 2) / 4)
+    d = 0.2 * np.exp(-((9 * x1 - 4) ** 2) - ((9 * x2 - 7) ** 2))
+    e = npr.normal(loc=0, scale=noise_var, size=a.shape)
+    #note if we don't truncate the gaussian, we won't see the effect of the noise 
+    #as all noise will be rescaled to the range 0,1.
+    e[np.where(e>1.0)]=1.0
+    e[np.where(e<-1.0)]=-1.0
+    return a + b + c - d + e, np.var(e)
 
 if __name__ == '__main__':
     print(create_data(100, 0.1))
